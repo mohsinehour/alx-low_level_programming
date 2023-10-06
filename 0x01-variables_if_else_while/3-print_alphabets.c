407769d25d699777567136c8c8b82ea0e38f54f3
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-06 15:02:24 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,16 @@
  *return alwys 0
  */
 int main() {
-    char lc, uc;
-    
-    for(lc = 'a'; lc<='z'; lc++)
-    {
-        putchar(lc);
-    }
-    for(uc = 'A'; uc<='Z'; uc++)
-    {
-        putchar(uc);
-    }
-    putchar('\n');
-    return 0;
+	char lc, uc;
+
+	for(lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for(uc = 'A'; uc <= 'Z'; uc++)
+	{
+	putchar(uc);
+	}
+	putchar('\n');
+	return 0;
 }
