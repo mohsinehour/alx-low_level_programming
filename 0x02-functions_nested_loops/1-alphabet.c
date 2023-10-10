2ef666f1c42b4c2e28165af32cf7dc47350e6246
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-10 08:56:17 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -  prints the alphabet, in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+		return (0);
+	}
+	return (0);
+}
