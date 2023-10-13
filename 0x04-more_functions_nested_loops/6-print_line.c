e2bcd305e2621ca086b4b713cf5fe7759fabc875
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-13 09:05:19 -0700

    Task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,18 @@
 
 void print_line(int n)
 {
-	int i;
-
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
-	
