3e8fac0d8c64cd0d7e842cb5f1e46f91e35f8190
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-13 09:09:05 -0700

    Task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,24 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
-
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('\\');
 		_putchar('\n');
-		for (j = 0; j < i; j++)
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
 	}
 }
