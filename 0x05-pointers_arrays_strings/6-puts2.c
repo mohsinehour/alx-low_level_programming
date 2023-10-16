1b75e7c1781ef6ad22b24a69cbe157f44d8345c6
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-16 13:19:56 -0700

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,23 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
 	{
-		_putchar(*str);
-		str = str + 2;
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
 	}
 	_putchar('\n');
 }
