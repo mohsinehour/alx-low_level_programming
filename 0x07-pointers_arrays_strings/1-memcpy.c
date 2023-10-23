ab3d22d12af9de471d4e5baef053769a4178b0f6
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-23 13:06:33 -0700

    Task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,12 @@
  * @src: Source parameter
  * @dest: Destination parameter
  * @n: number of bytes
- * Return: Copied memory
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i=0; i < n; i++)
 		dest = src[i];
