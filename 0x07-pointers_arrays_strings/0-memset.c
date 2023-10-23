6d37f4b416cfb502a3a36838d23af416af035bc6
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-23 08:48:31 -0700

    Task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-  * _memset - main function of the prototype
-  *
-  * @b: The value used to fill the memory
-  *
-  * @s: Function paramater
-  *
-  * @n: Function parameter
-  *
-  * Return: s
-  */
+ * _memset - main function of the prototype
+ *
+ * @b: The value used to fill the memory
+ *
+ * @s: Function paramater
+ *
+ * @n: Function parameter
+ *
+ * Return: s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
