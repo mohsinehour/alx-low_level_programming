8607940315e515b8958081197dcf0c1fe3ae7e0b
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-27 13:24:22 -0700

    Task0

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - main function prototype
+ * @src: Source parameter
+ * @dest: Destination parameter
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
