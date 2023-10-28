cee1977284e65d5435fd298f1cb307fa9e9c2d9f
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-27 17:01:59 -0700

    Task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include "main.h"		
+#include "main.h"
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- * Return: the int converted from the string		
+ * Return: the int converted from the string
  */
-int _atoi(char *s)		
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-	
-	i = 0;	
-	d = 0;	
+
+	i = 0;
+	d = 0;
 	n = 0;
-	len = 0;	
-	f = 0;		
+	len = 0;
+	f = 0;
 	digit = 0;
 	while (s[len] != '\0')
 		len++;
-	while (i < len && f == 0)	
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
@@ -32,30 +32,30 @@ int _atoi(char *s)
 				break;
 			f = 0;
 		}
-		i++;	
+		i++;
 	}
 	if (f == 0)
 		return (0);
-	return (n);		
-}		
+	return (n);
+}
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 (Success), 1 (Error)		
+ * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, char *argv[])	
-{	
+int main(int argc, char *argv[])
+{
 	int result, num1, num2;
 
-	if (argc < 3 || argc > 3)	
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
-		return (1);	
+		return (1);
 	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
-	return (0);		
+	return (0);
 }
