9b669a9b64f5cf3e6494d6618609a424f9f6b86e
mohsine29 <hourmatallahmohsine@gmail.com>
2023-10-31 07:51:12 -0700

    Task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(sizeof(int*) * height);
+	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
@@ -25,10 +25,8 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
-			{
+			for (; i >= 0; i--)
 				free(grid[i]);
-			}
 			free(grid);
 			return (NULL);
 		}
