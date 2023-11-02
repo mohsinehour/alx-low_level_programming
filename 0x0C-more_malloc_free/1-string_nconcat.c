d394056f3ac706515d1468e136e2c7f9574389e0
mohsine29 <hourmatallahmohsine@gmail.com>
2023-11-02 12:14:47 -0700

    Task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,22 +16,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1[i] = "";
-	else
-	{
-		while (s1[i] != '\0')
+	if (s2 == NULL)
+		s2[j] = "";
+	while (s1[i] != '\0')
 		{
 			i++;
 		}
-	}
-	if (s2 == NULL)
-		s2[j] = "";
-	else
-	{
-		while (s2[j] != '\0')
+	while (s2[j] != '\0')
 		{
 			j++;
 		}
-	}
 	if (n >= j)
 		n = j;
 	s = malloc(sizeof(char) * (i + j + 1));
