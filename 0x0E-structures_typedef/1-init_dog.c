3ee8ea318d61eba004bc96c609b83cafb50a8641
mohsine29 <hourmatallahmohsine@gmail.com>
2023-11-06 03:50:11 -0800

    Task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog.name = name;
-	dog.age = age;
-	dog.owner = owner;
+	if (d)
+	{
+		dog.name = name;
+		dog.age = age;
+		dog.owner = owner;
+	}
 }
