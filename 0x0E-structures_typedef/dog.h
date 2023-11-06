10ed2b7454ca3326e9ab350d0550a4b4592eadf0
mohsine29 <hourmatallahmohsine@gmail.com>
2023-11-06 03:14:51 -0800

    Task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains a dog infos
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
