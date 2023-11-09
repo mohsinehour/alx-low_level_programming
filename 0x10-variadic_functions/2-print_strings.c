53fe92ef31c3326452a70388ec4bae9406c60490
mohsine29 <hourmatallahmohsine@gmail.com>
2023-11-09 12:32:01 -0800

    Task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		char * x = va_arg(args, char *);
+		char *x = va_arg(args, char *);
 
 		if (x != NULL)
 			printf("%s", x);
