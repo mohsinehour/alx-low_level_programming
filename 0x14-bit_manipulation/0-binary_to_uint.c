0d2a2f7b816db3694fff471e1b0c3220625cb51d
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2023-11-23 10:33:57 -0800

    Task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		result = (result << 1) + (b[i] - '0');
 	}
