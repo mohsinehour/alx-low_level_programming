8606d2fee74fdee29b1f2702958a0d3fcc379498
mohsine29 <hourmatallahmohsine@gmail.com>
2024-01-15 12:59:21 -0800

    task2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+/**
+* add - adds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the sum
+*/
+
+int add(int i, int k)
+{
+	return (i + k);
+}
+
+/**
+* sub - subtracts two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the subtract
+*/
+
+int sub(int i, int k)
+{
+	return (i - k);
+}
+
+/**
+* mul - multiplies two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the multiple
+*/
+
+int mul(int i, int k)
+{
+	return (i * k);
+}
+
+/**
+* div - divideds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the dividend integer
+*/
+
+int div(int i, int k)
+{
+	if (k == 0)
+	{
+		fprintf(stderr, "Error: Division by zero\n");
+		return 0;
+	}
+	return (i / k);
+}
+
+/**
+  mod - finds the modulus of two integers
+  @i: first integer to add
+  @k: second integer to add
+  Return: the modulus
+*/
+
+int mod(int i, int k)
+{
+	if (k == 0)
+	{
+		fprintf(stderr, "Error: Modulo by zero\n");
+		return 0;
+	}
+	return (i % k);
+}
